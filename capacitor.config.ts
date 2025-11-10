a1d5fe2990344eea217902300d3cb0d521a1cfaa
Efrain793 <efrain.reynaavila@cesunbc.edu.mx>
2025-11-10 09:00:17 -0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'red-social-dancers',
+  webDir: 'www'
+};
+
+export default config;
